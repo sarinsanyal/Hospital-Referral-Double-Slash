589304867eceb81667ed997b0a3a3874212e2ffd
malay77patra <malay77patra@gmail.com>
2025-02-15 19:27:45 +0530

    working model

--- backend/db/models/User.js
+++ backend/db/models/User.js
@@ -21,6 +21,7 @@ const userSchema = new mongoose.Schema({
     }
 });
 
+
 // Hash password before saving
 userSchema.pre('save', async function (next) {
     if (!this.isModified('password')) return next();
