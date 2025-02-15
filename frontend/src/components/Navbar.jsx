3410a20a155490416dddbf8120997f81126b10e8
malay77patra <malay77patra@gmail.com>
2025-02-15 12:41:41 +0530

    kandkjad

---
+++ frontend/src/components/Navbar.jsx
@@ -0,0 +1,20 @@
+import { AppBar, Toolbar, Typography, Button } from "@mui/material";
+import { Link } from "react-router-dom";
+
+const Navbar = () => {
+  return (
+    <AppBar position="static">
+      <Toolbar>
+        <Typography variant="h6" sx={{ flexGrow: 1 }}>
+          MyApp
+        </Typography>
+        <Button color="inherit" component={Link} to="/">Home</Button>
+        <Button color="inherit" component={Link} to="/login">Login</Button>
+        <Button color="inherit" component={Link} to="/register">Register</Button>
+        <Button color="inherit" component={Link} to="/dashboard">Dashboard</Button>
+      </Toolbar>
+    </AppBar>
+  );
+};
+
+export default Navbar;
